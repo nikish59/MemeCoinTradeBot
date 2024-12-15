2cb820ffe5c8fd56a0b76fb09c2cce50604bcaee
Kharkovsky Nikita <nkharkovsky@yandex.ru>
2024-12-15 23:15:59 +0300

    "Add shinx docs"

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MemeCoinsTradeBot'
+copyright = '2024, nikish59'
+author = 'nikish59'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))  # Путь на один уровень выше папки docs
+extensions = [
+    'sphinx.ext.autodoc',    # Для извлечения docstring из Python-кода
+    'sphinx.ext.viewcode',   # Для отображения исходного кода
+    'sphinx.ext.napoleon'    # Для поддержки Google и NumPy стилей docstring
+]
+autoapi_dirs = ['../']  # Укажите путь к директории с вашим кодом
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
